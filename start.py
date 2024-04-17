5c1920746b03b67b57c29714c19d69e6205c6ce0
Python-luyi <119301610+yinjiuzui@users.noreply.github.com>
2024-04-17 17:20:40 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,3 +1,5 @@
 import os
-os.system('python download.py')
+base_path = './model'
+os.system(f'git clone https://code.openxlab.org.cn/MrCat/CatMediHealer_internlm.git {base_path}')
+os.system(f'cd {base_path} && git lfs pull')
 os.system('streamlit run app.py --server.address=0.0.0.0 --server.port 7860')
