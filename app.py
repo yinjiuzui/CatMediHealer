f84f8a7292d598b8fa443c72e7d543d1e015a720
Python-luyi <119301610+yinjiuzui@users.noreply.github.com>
2024-04-17 23:55:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
 # download internlm2 to the base_path directory using git tool
-base_path = './internlm2-chat-7b'
-os.system(f'git clone https://code.openxlab.org.cn/OpenLMLab/internlm2-chat-7b.git {base_path}')
+base_path = './CatMediHealer_internlm'
+os.system(f'git clone https://code.openxlab.org.cn/MrCat/CatMediHealer_internlm.git.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
@@ -16,8 +16,8 @@ def chat(message,history):
         yield response
 
 gr.ChatInterface(chat,
-                 title="InternLM2-Chat-7B",
+                 title="猫灵通识",
                 description="""
-InternLM is mainly developed by Shanghai AI Laboratory.  
+中文文化中，猫常常被视为拥有某种神秘力量的生物，能够带来好运，也象征着保护和治愈。结合 Medi 和 Healer ，CatMediHealer旨在传达出这是一个融合了中医智慧与现代大数据技术的医疗大模型，不仅能够深入挖掘和理解中医的复杂知识体系，还能为现代医疗提供智能化的辅助诊断和治疗建议，就如同一位拥有猫一般敏锐直觉的中医治疗专家。
                  """,
                  ).queue(1).launch()
