5c2674df0fafd0c1bd4c87083a2819bf5fd9a9ad
Python-luyi <119301610+yinjiuzui@users.noreply.github.com>
2024-04-18 21:27:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
 # download internlm2 to the base_path directory using git tool
 base_path = './CatMediHealer_internlm'
-os.system(f'git clone https://code.openxlab.org.cn/MrCat/CatMediHealer_internlm.git.git {base_path}')
+os.system(f'git clone https://code.openxlab.org.cn/MrCat/CatMediHealer_internlm.git{base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
