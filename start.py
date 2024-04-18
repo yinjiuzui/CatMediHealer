ba0bbec8a6ea216ac80ca771f1aa9fa539eac5be
Python-luyi <119301610+yinjiuzui@users.noreply.github.com>
2024-04-18 12:29:53 +0800

    Delete start.py

--- start.py
+++
@@ -1,5 +0,0 @@
-import os
-base_path = './model'
-os.system(f'git clone https://code.openxlab.org.cn/MrCat/CatMediHealer_internlm.git {base_path}')
-os.system(f'cd {base_path} && git lfs pull')
-os.system('streamlit run app.py --server.address=0.0.0.0 --server.port 7860')
