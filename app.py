ab66ec4607828c8c1f41d7b2e7adf6f8c7a5865d
Python-luyi <119301610+yinjiuzui@users.noreply.github.com>
2024-04-20 00:28:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,18 @@
 import gradio as gr
 import os
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
-# 设置基础路径
-base_path = os.path.abspath('CatMediHealer_internlm')
+# download internlm2 to the base_path directory using git tool
+base_path = './CatMediHealer_internlm'
+os.system(f'git clone https://code.openxlab.org.cn/MrCat/CatMediHealer_internlm.git {base_path}')
+os.system(f'cd {base_path} && git lfs pull')
 
-# 检查目录是否存在，如果存在，则跳过克隆步骤
-if not os.path.exists(base_path):
-    os.system(f'git clone https://code.openxlab.org.cn/MrCat/CatMediHealer_internlm.git {base_path}')
-    os.chdir(base_path)
-    os.system('git lfs pull')
+tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
 
-# Ensure you are in the correct directory if needed
-os.chdir(base_path)
-
-# Load the tokenizer and model
-tokenizer = AutoTokenizer.from_pretrained("/home/xlab-app-center/CatMediHealer_internlm/CatMediHealer_internlm", trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained("/home/xlab-app-center/CatMediHealer_internlm/CatMediHealer_internlm", trust_remote_code=True, torch_dtype=torch.float16).cuda()
-
-def chat(message, history):
-    for response, history in model.stream_chat(tokenizer, message, history, max_length=2048, top_p=0.7, temperature=1):
+def chat(message,history):
+    for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
         yield response
 
 gr.ChatInterface(chat,
