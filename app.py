a4c13ce1fd44525d36ad1ec9c025d2d91914852f
Python-luyi <119301610+yinjiuzui@users.noreply.github.com>
2024-05-25 01:37:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
 # download internlm2 to the base_path directory using git tool
-base_path = './CatMediHealer_internlm'
-os.system(f'git clone https://code.openxlab.org.cn/MrCat/CatMediHealer_internlm.git {base_path}')
+base_path = './huatuo2'
+os.system(f'git clone https://code.openxlab.org.cn/yinjiuzui/huatuo2.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
@@ -16,7 +16,7 @@ def chat(message,history):
         yield response
 
 gr.ChatInterface(chat,
-                 title="猫灵通识",
+                 title="健康咨询中医Chat",
                  description="""
-中文文化中，猫常常被视为拥有某种神秘力量的生物，能够带来好运，也象征着保护和治愈。结合 Medi 和 Healer ，CatMediHealer旨在传达出这是一个融合了中医智慧与现代大数据技术的医疗大模型，不仅能够深入挖掘和理解中医的复杂知识体系，还能为现代医疗提供智能化的辅助诊断和治疗建议，就如同一位拥有猫一般敏锐直觉的中医治疗专家。
+中医是中国传统文化的重要组成部分，拥有几千年的历史。中医强调整体观念和辩证施治，通过望、闻、问、切四诊合参，结合中药、针灸、推拿等多种治疗手段，以调节人体阴阳平衡，达到治病防病的目的。中医理论体系包括阴阳五行、气血津液、脏腑经络等，具有深厚的哲学基础和独特的诊疗方法。现代中医在传承的基础上不断发展，结合现代科技手段，致力于为人类健康提供更全面的服务。通过中医，可以更好地理解人体与自然的和谐关系，促进身心健康，提升生活质量。
                  """).queue(1).launch()
